4b9098e4e5e95e78380fd5af078718a49f6fa244
Yuki1005 <yhiramatsu5@icloud.com>
2021-10-03 00:01:10 +0900

    1

--- hello.py
+++
@@ -1,4 +0,0 @@
-#発展課題
-number1 = float(input("整数を入力してください\n"))
-number2 = float(input("割る数（整数）を入力してください\n"))
-print("商は" , int(number1 // number2) , "、余りは" , int(number1 % number2) , "です" , sep = '')
